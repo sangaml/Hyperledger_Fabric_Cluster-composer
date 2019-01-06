2099e32559f66867aa5d8e2de5b132b2f77a7d31
Sangam <36110390+sangaml@users.noreply.github.com>
2019-01-06 20:02:06 +0530

    Update hyperledger.sh

--- hyperledger.sh
+++ hyperledger.sh
@@ -27,8 +27,6 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | b
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-echo "[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"" >> ~/.bashrc
 nvm install v8.9 -y
 #Install node
 sudo apt install node-gyp -y
